2675f7957e438f5c9a3c96681d1085c854595413
JUDYLH <1455401322@QQ.COM>
2017-08-30 17:19:15 +0800

    Revert "Test"
    This reverts commit d11a3d2390b465b8be8099bf6142c96d083bd727.

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -1,5 +1,11 @@
 package test;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.crm.mapper.BasDictMapper;
+import com.crm.service.IBasService;
+
 public class Test {
 	
 	public static void main(String[] args) {

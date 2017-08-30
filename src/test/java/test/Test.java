bbd39e3d10acedab48554eaab04c68961aec6037
liuyuwei1989 <30585046+liuyuwei1989@users.noreply.github.com>
2017-08-30 16:19:08 +0800

    修改配置

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -4,15 +4,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.crm.mapper.BasDictMapper;
+import com.crm.service.IBasService;
 
 public class Test {
 	
 	public static void main(String[] args) {
 		
-		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml","springmvc-config.xml");
-		
-		BasDictMapper basDictMapper = (BasDictMapper)context.getBean("basDictMapper");
-		
-		System.out.println(basDictMapper.selectByPrimaryKey(2).getDictIsEditable());
+
 	}
 }

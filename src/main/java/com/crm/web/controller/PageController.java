9487932bea064182be132e82713adc301b0abd5b
liuyuwei1989 <30585046+liuyuwei1989@users.noreply.github.com>
2017-08-30 14:13:08 +0800

    一些底层修改。不影响使用

--- src/main/java/com/crm/web/controller/PageController.java
+++ src/main/java/com/crm/web/controller/PageController.java
@@ -4,13 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
+/**
+ * 用于页面链接
+ * @author hasee
+ */
 @Controller
 @RequestMapping("/page")
 public class PageController {
-	@RequestMapping("/{web}/{webSource}")
-	public ModelAndView index(@PathVariable("web") String web,@PathVariable("webSource") String webSource){
-		System.out.println("123123");
-		return new ModelAndView(web +"/"+webSource);
+	@RequestMapping("/{webDic}/{webFile}")
+	public ModelAndView goToWebpage(@PathVariable("webDic") String webDic,@PathVariable("webFile") String webFile){
+		return new ModelAndView(webDic +"/"+webFile);
 	}
 }
